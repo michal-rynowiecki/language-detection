fe46dc380b1ce60467713fce23108a9548a3466f
Michal Rynowiecki <michal@Michals-MacBook-Pro.local>
2026-04-07 18:29:18 +0200

    added args for script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #python detect.py -lm='google-bert/bert-base-multilingual-cased' -l='French,Danish'
 #python determine_string_length.py -lm='google-bert/bert-base-uncased' -l='Japanese'
 
-python determine_lang_length.py -lm='google-bert/bert-base-multilingual-cased'
+python determine_lang_length.py -lm='google-bert/bert-base-multilingual-cased' -r=10 -a=0.001
