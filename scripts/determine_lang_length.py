bf5c3a90855dd6cc129255d1f2a05e2c5c1534ac
Michal Rynowiecki <michal@Michals-MacBook-Pro.local>
2026-04-11 10:03:26 +0200

    typo fix

--- scripts/determine_lang_length.py
+++ scripts/determine_lang_length.py
@@ -44,6 +44,6 @@ def main():
 
     #calc_stats_single('/Users/michal/Projects/Thesis/data/lang_lengths/bert-base-multilingual-cased')
     #plot_bpc_vs_n('/Users/michal/Projects/Thesis/data/lang_lengths/bert-base-multilingual-cased/0.01_5_True_present.json')
-    €plot_true_vs_false('/Users/michal/Projects/Thesis/data/lang_lengths/bert-base-multilingual-cased/0.001_20_True_present.json', '/Users/michal/Projects/Thesis/data/lang_lengths/bert-base-multilingual-cased/0.001_20_False_present.json')
+    #plot_true_vs_false('/Users/michal/Projects/Thesis/data/lang_lengths/bert-base-multilingual-cased/0.001_20_True_present.json', '/Users/michal/Projects/Thesis/data/lang_lengths/bert-base-multilingual-cased/0.001_20_False_present.json')
 
 main()
